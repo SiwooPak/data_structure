cb27e6f4e22184a62b741ec024d83fe01b075103
SiwooPak <bogus1620@naver.com>
2021-06-27 17:26:44 +0900

    added: linkedlist

---
+++ linked_list/MyListNode.java
@@ -0,0 +1,23 @@
+package linked_list;
+
+public class MyListNode {
+	private String data; // 자료
+	public MyListNode next; // 링크
+	
+	public MyListNode() {
+		data = null;
+		next = null;
+	}
+	
+	public MyListNode(String data) {
+		this.data = data;
+		this.next = null;
+	}
+	
+	public MyListNode(String data, MyListNode link) {
+		this.data = data;
+		this.next = link;
+	}
+	
+	public String getData() { return data; }
+}
